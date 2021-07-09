d8a357850254e8ebf8edd9170817460578014333
DarkWeird <foxflameless@gmail.com>
2021-07-09 16:53:16 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components.

--- src/main/java/org/terasology/segmentedpaths/components/BlockMappingComponent.java
+++ src/main/java/org/terasology/segmentedpaths/components/BlockMappingComponent.java
@@ -1,27 +1,20 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.segmentedpaths.components;
 
-import org.terasology.engine.entitySystem.Component;
 import org.terasology.engine.math.Side;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 /**
  * Created by michaelpollind on 4/3/17.
  */
-public class BlockMappingComponent implements Component {
+public class BlockMappingComponent implements Component<BlockMappingComponent> {
     public Side s1;
     public Side s2;
+
+    @Override
+    public void copy(BlockMappingComponent other) {
+        this.s1 = other.s1;
+        this.s2 = other.s2;
+    }
 }
--- src/main/java/org/terasology/segmentedpaths/components/CurvedPathComponent.java
+++ src/main/java/org/terasology/segmentedpaths/components/CurvedPathComponent.java
@@ -1,33 +1,30 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.segmentedpaths.components;
 
 import org.joml.Vector3f;
-import org.terasology.engine.entitySystem.Component;
+import org.terasology.gestalt.entitysystem.component.Component;
 import org.terasology.reflection.MappedContainer;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by michaelpollind on 4/3/17.
  */
-public class CurvedPathComponent implements Component {
+public class CurvedPathComponent implements Component<CurvedPathComponent> {
     public List<CurvedPathComponent.CubicBezier> path;
     public Vector3f binormal;
     public float rotation;
+
+    @Override
+    public void copy(CurvedPathComponent other) {
+        this.path = other.path.stream()
+                .map(CubicBezier::copy)
+                .collect(Collectors.toList());
+        this.binormal = new Vector3f(other.binormal);
+        this.rotation = other.rotation;
+    }
 
     @MappedContainer
     public static class CubicBezier {
@@ -35,5 +32,14 @@ public class CurvedPathComponent implements Component {
         public Vector3f f2;
         public Vector3f f3;
         public Vector3f f4;
+
+        private CubicBezier copy() {
+            CubicBezier newCubicBezier = new CubicBezier();
+            newCubicBezier.f1 = f1;
+            newCubicBezier.f2 = f2;
+            newCubicBezier.f3 = f3;
+            newCubicBezier.f4 = f4;
+            return newCubicBezier;
+        }
     }
 }
